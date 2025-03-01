f22d60f200d13487b11c060c7425b886b9043640
Francesco Viccica <viccicafrancesco@gmail.com>
2025-03-01 11:46:53 +0100

    Esercitazione svolta

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = 2;
+
+let b = 5;
+
+let c = 10;
+
+let d = 25;
+
+let e = 50;
+
+let somma = a + b + c + d + e;
+
+let media = somma / 5;
+
+console.log( 'la somma tra i numeri equivale a ' + somma);
+
+console.log('la media equivale a ' + media);
